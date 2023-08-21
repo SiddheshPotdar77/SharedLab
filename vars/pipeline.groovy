ce70c8dd99268c3b6f9b94afd984887aa6cfba27
siddheshpotdar <141252264+SiddheshPotdar77@users.noreply.github.com>
2023-08-21 20:56:13 +0530

    Create pipeline.groovy

---
+++ vars/pipeline.groovy
@@ -0,0 +1,20 @@
+new sum(a,b)
+{
+  echo "Addition of ${a} and ${b} is ${a+b}"
+}
+
+new sub(a,b)
+{
+  echo "Subtraction of ${a} and ${b} is ${a+b}"
+}
+
+new mul(a,b)
+{
+  echo "Multiplication of ${a} and ${b} is ${a*b}"
+}
+
+new div(a,b)
+{
+  echo "Division of ${a} and ${b} is ${a/b}"
+}
+
